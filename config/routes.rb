b19c11ededfbbddcbb1495659017c059840debb8
satoshi0211wata <satoshi.2.11@gmail.com>
2019-12-10 13:56:13 +0900

    プロフィールページ（フロントマークアップ）
    UsersのShowアクションで呼び出せるページです。show.html.hamlファイル、show.scssファイル作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "items#index"
+  root to: "users#index"
+  resources :users, only: [:index,:show]
+  resources :items, only: [:index]
 end
