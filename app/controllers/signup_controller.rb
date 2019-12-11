fa4a49f577be6afc2a21639ae47e51443c42934e
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-11 15:44:47 +0900

    sessionで次のページへデータ持ってこれる

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -5,26 +5,48 @@ class SignupController < ApplicationController
 
   def step2
     # step1で入力された値をsessionに保存
-    session[:nickname] = user_path[:nickname]
-  #   session[:email] = user_params[:email]
-  #   session[:encrypted_password] = user_params[:encrypted_password]
-  #   session[:family_name] = user_params[:family_name]
-  #   session[:first_name] = user_params[:first_name]
-  #   session[:family_name_kana] = user_params[:family_name_kana]
-  #   session[:first_name_kana] = user_params[:first_name_kana]
-  #   @user = User.new # 新規インスタンス作成
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:encrypted_password] = user_params[:encrypted_password]
+    session[:family_name] = user_params[:family_name]
+    session[:first_name] = user_params[:first_name]
+    session[:family_name_kana] = user_params[:family_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    @user = User.new # 新規インスタンス作成
   end
 
-  # def step3
-  #   # step2で入力された値をsessionに保存
-  #   session[:phone_number] = user_params[:phone_number]
-  #   @user = User.new
+  def step3
+    # step2で入力された値をsessionに保存
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
+  end
+
+  # def step4
+  #   session[:]
   # end
+  def create
+    @user = User.new(
+      nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
+      encrypted_password: session[:encrypted_password],
+      family_name: session[:family_name], 
+      first_name: session[:first_name], 
+      family_name_kana: session[:family_name_kana], 
+      first_name_kana: session[:first_name_kana], 
+    )
+    if @user.save
+      # ログインするための情報を保管
+      session[:id] = @user.id
+      redirect_to done_signup_index_path
+    else
+      render '/signup/step1'
+    end
+  end
 
 
 
   private
-　# 許可するキーを設定します
+# 許可するキーを設定します
   def user_params
     params.require(:user).permit(
       :nickname, 
@@ -34,6 +56,6 @@ class SignupController < ApplicationController
       :first_name, 
       :family_name_kana, 
       :first_name_kana, 
-  )
+    )
   end
 end
