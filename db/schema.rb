98e407cd574a22925a685cd7c89883661c81e5b9
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-16 17:55:42 +0900

    電話番号の登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191212051439) do
+ActiveRecord::Schema.define(version: 20191216075057) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postal_code"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20191212051439) do
     t.integer  "birth_year"
     t.integer  "birth_month"
     t.integer  "birth_day"
-    t.integer  "phone_number"
+    t.string   "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
