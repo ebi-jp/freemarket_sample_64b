29a981b2e65139e7a6e407bec0b20ddd73cfd25b
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-17 18:01:12 +0900

    本番で試すため

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -7,7 +7,7 @@ class CardController < ApplicationController
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = 'sk_test_dc96a7044e1dc7f4ef27fc54'
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
@@ -28,7 +28,7 @@ class CardController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = 'sk_test_dc96a7044e1dc7f4ef27fc54'
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
@@ -41,7 +41,7 @@ class CardController < ApplicationController
     if card.blank?
       redirect_to action: "new" 
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = 'sk_test_dc96a7044e1dc7f4ef27fc54'
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
