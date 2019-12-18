e51f3b60931968a8bc21f4acb980fcd496dc9106
kyouichi-inoue <corleonis81@gmail.com>
2019-12-18 16:02:08 +0900

    途中経過model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
-  belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  # belongs_to :seller, class_name: "User"
+  # belongs_to :buyer, class_name: "User"
+  # # belongs_to :brand
+  # belongs_to :category
+  has_many :images, dependent: :destroy
+  # mount_uploader :image, ImageUploader
+  accepts_nested_attributes_for :images
 end
