60433223419ede529eeaf46491a84137e14d0efc
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-19 19:59:06 +0900

    確認

--- db/schema.rb
+++ db/schema.rb
@@ -25,17 +25,17 @@ ActiveRecord::Schema.define(version: 20191217045840) do
     t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
   end
 
-  # create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-  #   t.string "brand"
-  # end
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "brand"
+  end
 
-  # create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-  #   t.integer  "user_id",     null: false
-  #   t.string   "customer_id", null: false
-  #   t.string   "card_id",     null: false
-  #   t.datetime "created_at",  null: false
-  #   t.datetime "updated_at",  null: false
-  # end
+  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",     null: false
+    t.string   "customer_id", null: false
+    t.string   "card_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "category"
