1e0bb906784af3ea7855b0769dba64476e5f4368
kyouichi-inoue <corleonis81@gmail.com>
2019-12-19 14:20:19 +0900

    model作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,10 @@ class Item < ApplicationRecord
   # # belongs_to :brand
   # belongs_to :category
   has_many :images, dependent: :destroy
-  # mount_uploader :image, ImageUploader
   accepts_nested_attributes_for :images
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :parent
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :hutan
+  belongs_to_active_hash :days
 end
