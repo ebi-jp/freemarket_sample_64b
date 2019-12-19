c546036121ae96b7a8f7a7f9f4cab6fcc6803cf9
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-19 19:48:10 +0900

    確認

--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,13 @@ ActiveRecord::Schema.define(version: 20191217045840) do
     t.string "brand"
   end
 
-  create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",     null: false
-    t.string   "customer_id", null: false
-    t.string   "card_id",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+  # create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  #   t.integer  "user_id",     null: false
+  #   t.string   "customer_id", null: false
+  #   t.string   "card_id",     null: false
+  #   t.datetime "created_at",  null: false
+  #   t.datetime "updated_at",  null: false
+  # end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "category"
