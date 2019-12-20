38ce3ab6d27170698ac87fc570385abfd6fb47b9
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-20 18:52:30 +0900

    購入機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#index"
   resources :users, only: [:index,:show,:new]
-  resources :items, only: [:index,:new,:create,:show]
-  resources :purchase, only: [:index]
+  resources :items, only: [:index,:new,:create,:show]do
+    resources :purchase, only: [:index] do
+      collection do
+        get 'index', to: 'purchase#index'
+        post 'pay', to: 'purchase#pay'
+        get 'done', to: 'purchase#done'
+    end
+  end
+end
 
   resources :signup do
     collection do
@@ -33,13 +40,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :purchase, only: [:index] do
-    collection do
-      get 'index', to: 'purchase#index'
-      post 'pay', to: 'purchase#pay'
-      get 'done', to: 'purchase#done'
-    end
-  end
-  
+
 
 end
