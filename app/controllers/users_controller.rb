38bcfd2540cf8a38e626875b74ca1dd128f76639
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-24 00:12:46 +0900

    本人情報ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def edit
+    @user  =User.find(params[:id])
+  end
+  def update
+    user = User.find(params{:id})
+    user.update(user_params)
+  end
 end
