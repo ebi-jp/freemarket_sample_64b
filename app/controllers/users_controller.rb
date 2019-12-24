1d9f862d95765d8cf15973852864288020767fd6
ebi-jp <57351121+ebi-jp@users.noreply.github.com>
2019-12-24 12:41:22 +0900

    ユーザー情報

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,5 @@ class UsersController < ApplicationController
   def edit
     @user  =User.find(params[:id])
   end
-  def update
-    user = User.find(params{:id})
-    user.update(user_params)
-  end
+
 end
