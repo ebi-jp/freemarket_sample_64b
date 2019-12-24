fbccf2b676e91fbae26d970a53f7b2675e25b4ff
kyouichi-inoue <corleonis81@gmail.com>
2019-12-24 15:42:48 +0900

    商品削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   root to: "users#index"
   resources :users, only: [:index,:show,:new]
 
-  resources :items, only: [:index,:new,:create,:show,:edit,:update]do
+  resources :items, only: [:index,:new,:create,:show,:edit,:update,:destroy]do
     resources :purchase, only: [:index] do
       collection do
         get 'index', to: 'purchase#index'
