686bf187e43301afc70e73cd575ab438e2c69f69
satoshi0211wata <satoshi.2.11@gmail.com>
2019-12-25 14:37:15 +0900

    マイページ表示方法の変更に伴う修正
    アクションをユーザーコントロール内に。データの引き渡しの関係に伴う関連ファイルの修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+
+
   def index
     # binding.pry
     # @item = Item.find(params[:item_id])
@@ -6,14 +8,28 @@ class UsersController < ApplicationController
     @items2 = Item.limit(1).order('id DESC')
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def mypage
+    @user = User.find(params[:id])
+  end
+
   def show
+    @user = User.find(params[:id])
   end
 
   def logout
+    @user = User.find(params[:id])
   end
 
-  def edit
-    @user  =User.find(params[:id])
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+
+  # def mypage
+  #   @user = User.find(params[:id])
+  # end
 
 end
